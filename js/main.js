3af31332a2893d0ff136ecb85300820e4f1074b2
Abhijeet <s530670@nwmissouri.edu>
2017-09-27 21:46:52 -0500

    Initial setup

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+ function submitForm(){
+    var name = document.getElementById("name").value;
+    var phone = document.getElementById("phoneNo").value;
+    var query = document.getElementById("query").value;
+    alert("Hi " + name +','+
+            "\nWill reach you @ " + phone +
+            "\nWe received your queries about " + query);
+}
+
+function calculateRate(){
+        alert("Development is In-progress..\n Will back to you soon..")
+}
